a1ac4f2459d18dfac70fcb24886afec0b93f5c36
PhilibertJens <jens.philibert@student.howest.be>
2017-11-23 15:16:45 +0100

    Verschillende oefeningen gemaakt.

---
+++ 04-HTMLGetallen/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _04_HTMLGetallen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            for (int i = 1; i < 6; i++)
+            {
+                //if (i == 3) continue; //3 wordt overgeslaan. De loop gaat verder met 4.
+                //if (i == 3) break; //De loop stopt bij 3.
+                lstGetallen.Items.Add(i);
+            }
+        }
+
+        private void lstGetallen_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            string feedback;
+            string getalVoluit;
+            int gekozenGetal = (int)lstGetallen.SelectedValue;
+            switch (gekozenGetal)
+            {
+                case 1:
+                    getalVoluit = "een";
+                    break;
+                case 2:
+                    getalVoluit = "twee";
+                    break;
+                case 3:
+                    getalVoluit = "drie";
+                    break;
+                case 4:
+                    getalVoluit = "vier";
+                    break;
+                default:
+                    getalVoluit = "vijf";
+                    break;
+            }
+            feedback = "Het getal " + gekozenGetal + " schrijf je zo: " + getalVoluit;
+            lblGetalVoluit.Content = feedback;
+        }
+    }
+}
--- Sliders.Wpf/MainWindow.xaml.cs
+++ Sliders.Wpf/MainWindow.xaml.cs
@@ -20,17 +20,43 @@ namespace Sliders.Wpf
     /// </summary>
     public partial class MainWindow : Window
     {
-        int getal1, getal2;
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void btnTestSliders_Click(object sender, RoutedEventArgs e)
+        string VergelijkGetallen(int getal1, int getal2)
         {
-            
+            string feedback = "";
+            if (getal1 == getal2) feedback = getal1 + " (slider1) is gelijk aan " + getal2 + " (slider2)";
+            else if (getal1 > getal2) feedback = getal1 + " (slider1) is groter dan " + getal2 + " (slider2)";
+            else feedback = getal2 + " (slider2) is groter dan " + getal1 + " (slider1)";
+            return feedback;
         }
 
+        private void sldGetal1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int getal1 = (int)sldGetal1.Value;
+            int getal2 = (int)sldGetal2.Value;
+            lblBoodschap.Content = VergelijkGetallen(getal1, getal2);
+            if (getal1 < getal2) sldGetal2.Value = sldGetal1.Value;
+        }
+
+
+        private void btnTestSliders_Click(object sender, RoutedEventArgs e)
+        {
+            //int getal1 = (int)sldGetal1.Value;
+            //int getal2 = (int)sldGetal2.Value;
+            //lblBoodschap.Content = VergelijkGetallen(getal1, getal2);
+        }
+
+        private void sldGetal2_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int getal1 = (int)sldGetal1.Value;
+            int getal2 = (int)sldGetal2.Value;
+            lblBoodschap.Content = VergelijkGetallen(getal1, getal2);
+            if (getal2 > getal1) sldGetal1.Value = sldGetal2.Value;
+        }
     }
 }
